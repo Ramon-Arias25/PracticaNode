0be975814594777718b3cac1b5e8cdcb9f60e300
Ramon-Arias25 <56901379+Ramon-Arias25@users.noreply.github.com>
2020-03-13 13:36:11 -0300

    Respaldo Practica node

---
+++ src/server/config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const Handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const morgan = require('morgan');
+const multer = require('multer');
+
+const routes = require('../routes/index');
+const errorhandler = require('errorhandler');
+
+module.exports = app => {
+    
+    //settings process.env.PORT ||
+    app.set('port',  3000);
+    app.set('views', path.join(__dirname, '../views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'),'partials'),
+        layoutsDir: path.join(app.get('views'),'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers'),
+        handlebars: allowInsecurePrototypeAccess(Handlebars)
+    }));
+    app.set('view engine', '.hbs');
+
+    //middlewares
+    app.use(morgan('dev'));
+    app.use(multer({dest: path.join(__dirname,'../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json());
+
+    //routes
+    routes(app);
+
+    //static files
+    app.use('/public', express.static(path.join(__dirname, '../public')))
+
+    //errorhadlers
+    if ('development' === app.get('env')){
+        app.use(errorhandler);
+    }
+
+    return app;
+}
